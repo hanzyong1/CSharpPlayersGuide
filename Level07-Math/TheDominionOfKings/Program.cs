071834b2449d4ca2d76ef78db9c95484d2a17039
zhiyong <zhiyong@codongenomics.com>
2024-04-28 19:39:41 +0800

    Level 07 - The Dominion Of Kings

---
+++ Level07-Math/TheDominionOfKings/Program.cs
@@ -0,0 +1,15 @@
+﻿int pointPerEstate = 1;
+int pointPerDuchy = 3;
+int pointPerProvince = 6;
+
+Console.WriteLine("How many estates do you have?");
+int numberOfEstates = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("How many duchies do you have?");
+int numberOfDuchies = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("How many provinces do you have?");
+int numberOfProvinces = Convert.ToInt32(Console.ReadLine());
+
+int total = (numberOfEstates * pointPerEstate) + (numberOfDuchies * pointPerDuchy) + (numberOfProvinces * pointPerProvince);
+Console.WriteLine($"Your points: {total}");
