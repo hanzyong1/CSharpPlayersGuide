5fa4b01f663994cd9ffc7929647c9d98724c2eeb
zhiyong <zhiyong@codongenomics.com>
2024-04-28 19:32:25 +0800

    Level 07 - The Four Sisters And The Duckbear

---
+++ Level07-Math/TheFourSistersAndTheDuckbear/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("What is the number of chocolate eggs gathered today?");
+int numberOfChocolateEggs = Convert.ToInt32(Console.ReadLine());
+
+int numberOfSisters = 4;
+int chocolateEggsForDuckbear;
+int chocolateEggsForEachSister;
+
+chocolateEggsForDuckbear = numberOfChocolateEggs % numberOfSisters;
+
+chocolateEggsForEachSister = (numberOfChocolateEggs - chocolateEggsForDuckbear) / numberOfSisters;
+
+Console.WriteLine($"{chocolateEggsForDuckbear} chocolate eggs for duckbear.");
+Console.WriteLine($"{chocolateEggsForEachSister} chocolate eggs for each sister.");
