43cab95ac8044c5ed874cc123059659764f3ecec
zhiyong <zhiyong@codongenomics.com>
2024-05-28 21:28:09 +0800

    Level08-Defense of Consolas

---
+++ Level08-Console2.0/TheDefenseOfConsolas/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Title = "Defense of Consolas";
+
+Console.Write("Target Row? ");
+int targetRow = Convert.ToInt32(Console.ReadLine());
+Console.Write("Target Column? ");
+int targetColumn = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Deploy to:");
+Console.WriteLine($"({targetRow}, {targetColumn - 1})");
+Console.WriteLine($"({targetRow - 1}, {targetColumn})");
+Console.WriteLine($"({targetRow}, {targetColumn + 1})");
+Console.WriteLine($"({targetRow + 1}, {targetColumn})");
+
+Console.ForegroundColor = ConsoleColor.Red;
+
+Console.Beep();
