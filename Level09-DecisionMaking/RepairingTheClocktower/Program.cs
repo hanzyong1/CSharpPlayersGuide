07108f81deb1c94cbde68e46a9dc07c94b2c8842
zhiyong <zhiyong@codongenomics.com>
2024-05-28 22:08:26 +0800

    Level 09 - Repairing the Clocktower

---
+++ Level09-DecisionMaking/RepairingTheClocktower/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Input a number");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine("Tick");
+} else
+{
+    Console.WriteLine("Tock");
+}
