ab184f95c332f52f91f68093aea8103857a5cd4b
zhiyong <zhiyong@codongenomics.com>
2024-05-28 22:17:05 +0800

    Level 09 - Watchtower

---
+++ Level09-DecisionMaking/Watchtower/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Write("x value? ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("y value? ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x < 0 && y > 0)
+{
+    Console.WriteLine("The enemy is to the northwest!");
+} else if (x == 0 && y > 0)
+{
+    Console.WriteLine("The enemy is to the north!");
+} else if (x > 0 && y > 0)
+{
+    Console.WriteLine("The enemy is to the northeast!");
+} else if (x < 0 && y == 0)
+{
+    Console.WriteLine("The enemy is to the west!");
+} else if (x == 0 && y == 0)
+{
+    Console.WriteLine("The enemy is here!");
+} else if (x > 0 && y == 0)
+{
+    Console.WriteLine("The enemy is to the east!");
+} else if (x < 0 && y < 0)
+{
+    Console.WriteLine("The enemy is to the southwest!");
+} else if (x == 0 && y < 0)
+{
+    Console.WriteLine("The enemy is to the south!");
+} else if (x > 0 && y < 0)
+{
+    Console.WriteLine("The enemy is to the southeast!");
+}
