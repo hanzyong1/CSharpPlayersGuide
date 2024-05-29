2747f51d30718444d1369a1c117ef059305b3c2c
zhiyong <zhiyong@codongenomics.com>
2024-05-29 17:24:40 +0800

    Level 10 - The Prototype

---
+++ Level11-Looping/ThePrototype/Program.cs
@@ -0,0 +1,31 @@
+﻿int pilotNumber = -1;
+
+while (pilotNumber < 0 || pilotNumber > 100)
+{
+    Console.Write("User 1, enter a number between 0 and 100: ");
+    pilotNumber = Convert.ToInt32(Console.ReadLine());
+
+    Console.Clear();   
+};
+
+Console.WriteLine("User 2, guess the number.");
+int hunterNumber = 0;
+
+while (hunterNumber != pilotNumber)
+{
+    Console.Write("What is your next guess? ");
+    hunterNumber = Convert.ToInt32(Console.ReadLine());
+
+    if (hunterNumber > pilotNumber)
+    {
+        Console.WriteLine($"{hunterNumber} is too high.");
+    }
+    else if (hunterNumber < pilotNumber)
+    {
+        Console.WriteLine($"{hunterNumber} is too low");
+    }
+    else
+    {
+        Console.WriteLine("You guessed the number!");
+    }
+}
