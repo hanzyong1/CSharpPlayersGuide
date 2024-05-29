7d96977d10b63b6d4bb4ec906b46c8289ae375ef
zhiyong <zhiyong@codongenomics.com>
2024-05-29 17:31:43 +0800

    Level 10 - The Magic Cannon

---
+++ Level11-Looping/TheMagicCannon/Program.cs
@@ -0,0 +1,23 @@
+﻿for (int i = 1; i <= 100; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine($"{i}: Combined");
+    }
+    else if (i % 3 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"{i}: Fire");
+    } 
+    else if (i % 5 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine($"{i}: Electric");
+    } 
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Gray;
+        Console.WriteLine($"{i}: Normal");
+    }
+}
