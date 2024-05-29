13a005a19dd4894dff2b9bcb678e51469051565e
zhiyong <zhiyong@codongenomics.com>
2024-05-29 20:36:31 +0800

    Level 12 - The Replicator of D'To

---
+++ Level12-Arrays/TheReplicatorOfDTo/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] oldArray = new int[5];
+int[] newArray = new int[5];
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine("Please input a number: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    oldArray[i] = num;
+}
+
+for (int i = 0; i < oldArray.Length; i++)
+{
+    newArray[i] = oldArray[i];
+    Console.WriteLine($"Old Array: {oldArray[i]}");
+    Console.WriteLine($"New Array: {newArray[i]}");
+}
