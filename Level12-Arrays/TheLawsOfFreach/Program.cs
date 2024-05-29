696641d72811cd2badd91541b0df823c2a16e3b9
zhiyong <zhiyong@codongenomics.com>
2024-05-29 20:54:05 +0800

    Level 12 - The Laws of Freach

---
+++ Level12-Arrays/TheLawsOfFreach/Program.cs
@@ -0,0 +1,21 @@
+﻿// minimum
+int[] array = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+int currentSmallest = int.MaxValue; // Start higher than anything in the array.
+foreach (int num in array)
+{
+    if (num < currentSmallest)
+    {
+        currentSmallest = num;
+    }
+}
+Console.WriteLine(currentSmallest);
+
+// average
+int[] array2 = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+int total = 0;
+foreach (int num in array2)
+{
+    total += num;
+}
+float average = (float)total / array2.Length;
+Console.WriteLine(average);
