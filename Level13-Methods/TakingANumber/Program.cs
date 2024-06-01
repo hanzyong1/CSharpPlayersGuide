16649d18818624c9c0a9b94dff657e6ee23a0210
zhiyong <zhiyong@codongenomics.com>
2024-06-01 20:22:53 +0800

    Level 13 - Taking A Number

---
+++ Level13-Methods/TakingANumber/Program.cs
@@ -0,0 +1,20 @@
+﻿int AskForNumber(string text)
+{
+    Console.WriteLine(text);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int AskForNumberInRange(string text, int min, int max)
+{
+    while (true)
+    {
+        int num = AskForNumber(text);
+        if (num > min && num < max)
+        {
+            return num;
+        }
+    }
+}
+
+AskForNumberInRange("input number", 1, 10);
