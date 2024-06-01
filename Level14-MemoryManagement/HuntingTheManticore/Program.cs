e89c6102499824b73466b788dc60d26ed67efe6a
zhiyong <zhiyong@codongenomics.com>
2024-06-01 23:06:25 +0800

    Level 14 - Hunting The Manticore

---
+++ Level14-MemoryManagement/HuntingTheManticore/Program.cs
@@ -0,0 +1,86 @@
+﻿int rangeOfManticoreFromTheCity;
+int cityFullHealth = 15;
+int cityCurrentHealth = 15;
+int manticoreFullHealth = 10;
+int manticoreCurrentHealth = 10;
+int currentRound = 1;
+
+int getRangeOfManticoreFromTheCity()
+{
+    Console.Write("Player 1, how far away from the city do you want to station the Manticore? ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int getCannonTargetRange()
+{
+    Console.Write("Enter desired cannon range: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int getCannonDamage(int roundNumber)
+{
+    if (roundNumber % 3 == 0 && roundNumber % 5 == 0)
+    {
+        return 10;
+    } 
+    else if (roundNumber % 3 == 0)
+    {
+        return 3;
+    } 
+    else if (roundNumber % 5 == 0)
+    {
+        return 3;
+    } 
+    else
+    {
+        return 1;
+    }
+}
+
+void getResultOfCannonShot()
+{
+    int cannonTargetRange = getCannonTargetRange();
+    if (cannonTargetRange > rangeOfManticoreFromTheCity)
+    {
+        cityCurrentHealth -= 1;
+        Console.WriteLine("That round OVERSHOT the target.");
+    } 
+    else if (cannonTargetRange < rangeOfManticoreFromTheCity)
+    {
+        cityCurrentHealth -= 1;
+        Console.WriteLine("That round FELL SHORT the target.");
+    } 
+    else
+    {
+        cityCurrentHealth -= 1;
+        manticoreCurrentHealth -= 1;
+        Console.WriteLine("That round was a DIRECT HIT!");
+    }
+}
+
+void playRound()
+{
+    Console.WriteLine("---------------------------------------------------");
+    Console.WriteLine($"STATUS: Round: {currentRound} City: {cityCurrentHealth}/{cityFullHealth} Manticore: {manticoreCurrentHealth}/{manticoreFullHealth}");
+    Console.WriteLine($"The cannon is expected to deal {getCannonDamage(currentRound)} damage this round.");
+    getResultOfCannonShot();
+    currentRound += 1;
+}
+
+rangeOfManticoreFromTheCity = getRangeOfManticoreFromTheCity();
+Console.Clear();
+Console.WriteLine("Player 2, it is your turn.");
+while (true)
+{
+    playRound();
+    if (cityCurrentHealth <= 0)
+    {
+        Console.WriteLine("The city of Consolas has been destroyed!");
+        return;
+    }
+    else if (manticoreCurrentHealth <= 0)
+    {
+        Console.WriteLine("The Manticore has been destroyed! The city of Consolas has been saved!");
+        return;
+    }
+}
