c2c328b93e8b53a30032a4fe1fa2c7bfdfe500ee
zhiyong <zhiyong@codongenomics.com>
2024-06-02 22:02:55 +0800

    Level 16 - Simula's Test

---
+++ Level16-Enumerations/SimulasTest/Program.cs
@@ -0,0 +1,39 @@
+﻿State currentState = State.Locked;
+
+void manipulateChest(string input)
+{
+    if (currentState == State.Open && input == "close")
+    {
+        currentState = State.Closed;
+    }
+    else if (currentState == State.Closed && input == "lock") 
+    {
+        currentState = State.Locked;
+    }
+    else if (currentState == State.Closed && input == "open")
+    {
+        currentState = State.Open;
+    }
+    else if (currentState == State.Locked && input == "unlock")
+    {
+        currentState = State.Closed;
+    }
+    else
+    {
+        return;
+    }
+}
+
+while (true)
+{
+    Console.Write($"The chest is {currentState}. What do you want to do? ");
+    string input = Console.ReadLine();
+    manipulateChest(input);
+}
+
+enum State
+{
+    Locked,
+    Open,
+    Closed,
+}
