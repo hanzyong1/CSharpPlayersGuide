23bf18a7c322df9c4865fb0592f9281b08ec2edd
zhiyong <zhiyong@codongenomics.com>
2024-06-12 12:08:22 +0800

    Level 24 - The Point

---
+++ Level24-CatacombsOfTheClass/ThePoint/Program.cs
@@ -0,0 +1,24 @@
+﻿Point a = new Point(2, 3);
+Point b = new Point(-4, 0);
+
+Console.WriteLine($"({a.X}, {a.Y})");
+Console.WriteLine($"({b.X}, {b.Y})");
+
+public class Point
+{
+    public int X { get; }
+    public int Y { get; }
+
+    public Point()
+    {
+        X = 0;
+        Y = 0;
+    }
+
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+}
+
