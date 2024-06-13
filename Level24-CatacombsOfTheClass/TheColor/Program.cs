adcea15b5acfec0d51dc81dcd20afbd3229990d6
zhiyong <zhiyong@codongenomics.com>
2024-06-13 16:36:49 +0800

    Level 24 - The Color

---
+++ Level24-CatacombsOfTheClass/TheColor/Program.cs
@@ -0,0 +1,28 @@
+﻿Color custom = new Color(1, 1, 1);
+Color white = Color.White;
+
+Console.WriteLine($"({custom.R}, {custom.G}, {custom.B})");
+Console.WriteLine($"({white.R}, {white.G}, {white.B})");
+
+public class Color
+{
+    public int R { get; }
+    public int G { get; }
+    public int B { get; }
+
+    public Color(int red, int green, int blue)
+    {
+        R = red;
+        G = green;
+        B = blue;
+    }
+
+    public static Color White = new Color(255, 255, 255);
+    public static Color Black = new Color(0, 0, 0);
+    public static Color Red = new Color(255, 0, 0);
+    public static Color Orange = new Color(255, 165, 0);
+    public static Color Yellow = new Color(255, 255, 0);
+    public static Color Green = new Color(0, 128, 0);
+    public static Color Blue = new Color(0, 0, 255);
+    public static Color Purple = new Color(128, 0, 128);
+}
