96ef13d0db5c0ef43cc04503253d044b332914cf
zhiyong <zhiyong@codongenomics.com>
2024-06-18 14:39:31 +0800

    Level 29 - War Preparations

---
+++ Level29-Records/WarPreparations/Program.cs
@@ -0,0 +1,28 @@
+﻿Sword original = new Sword(Material.Iron, Gemstone.None, 10, 5);
+Sword fancy = original with { Material = Material.Binarium, Gemstone = Gemstone.Diamond };
+Sword ultralong = original with { Material = Material.Binarium, Gemstone = Gemstone.Sapphire, Length = 15 };
+
+Console.WriteLine(original);
+Console.WriteLine(fancy);
+Console.WriteLine(ultralong);
+
+public record Sword(Material Material, Gemstone Gemstone, float Length, float Width);
+
+public enum Material
+{
+    Wood,
+    Bronze,
+    Iron,
+    Steel,
+    Binarium,
+}
+
+public enum Gemstone
+{
+    Emerald,
+    Amber,
+    Sapphire,
+    Diamond,
+    Bitstone,
+    None,
+}
