ab732f09c95fa48968184220359f0cce97004bf3
zhiyong <zhiyong@codongenomics.com>
2024-06-18 15:49:22 +0800

    Level 30 - Colored Items

---
+++ Level30-Generics/ColoredItems/Program.cs
@@ -0,0 +1,33 @@
+﻿ColoredItem<Sword> sword = new ColoredItem<Sword>(new Sword(), ConsoleColor.Blue);
+ColoredItem<Bow> bow = new ColoredItem<Bow>(new Bow(), ConsoleColor.Red);
+ColoredItem<Axe> axe = new ColoredItem<Axe>(new Axe(), ConsoleColor.Green);
+
+sword.Display();
+bow.Display();
+axe.Display();
+
+public class Sword { }
+public class Bow { }
+public class Axe { }
+
+public class ColoredItem<T>
+{
+    public T Item { get; set; }
+    public ConsoleColor ConsoleColor { get; }
+
+    public ColoredItem(T item, ConsoleColor color) 
+    {
+        Item = item;
+        ConsoleColor = color;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine(ToString());
+    }
+
+    public override string ToString()
+    {
+        return $"A {ConsoleColor} {Item}";
+    }
+}
