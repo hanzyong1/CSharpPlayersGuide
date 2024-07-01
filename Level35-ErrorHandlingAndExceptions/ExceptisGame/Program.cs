1846ced2371b02770fd4638e805595f57d527d28
zhiyong <zhiyong@codongenomics.com>
2024-07-01 14:49:51 +0800

    Level 35 - Excepti's Game

---
+++ Level35-ErrorHandlingAndExceptions/ExceptisGame/Program.cs
@@ -0,0 +1,34 @@
+﻿try
+{
+    int answer = new Random().Next(10);
+    List<int> previousGuesses = new List<int>();
+
+    while (true)
+    {
+        int input;
+        bool previouslyGuessed;
+
+        do
+        {
+            Console.WriteLine("Pick a number between 0 and 9 (inclusive)");
+            input = Convert.ToInt32(Console.ReadLine());
+            previouslyGuessed = previousGuesses.Contains(input);
+            if (previouslyGuessed)
+            {
+                Console.WriteLine("That number has been guessed before.");
+            }
+        } 
+        while (previouslyGuessed);
+
+        if (input == answer)
+        {
+            throw new Exception();
+        }
+
+        previousGuesses.Add(input);
+    }
+}
+catch
+{
+    Console.WriteLine("That was a bad number! You lose!");
+}
